4061bc5623a82184e33fadd20408e7c260bbfe15
MattYamamoto <matt.yamamoto@gmail.com>
2015-11-14 21:09:03 -0800

    Create key map object
    Create object to hold key labels.
    Move key labels into the object.

--- ziplines/calculator/js/calculator.js
+++ ziplines/calculator/js/calculator.js
@@ -1,8 +1,201 @@
 $(document).ready(function() {
-  var  $key = $('.key-button')
+  var $key = $('.key-button'),
+    keyMap = {
+      "k07": {
+        "main": "",
+        "alt1": "",
+        "alt2": ""
+      },
+      "k08": {
+        "main": "",
+        "alt1": "",
+        "alt2": ""
+      },
+      "k09": {
+        "main": "",
+        "alt1": "",
+        "alt2": ""
+      },
+      "k10": {
+        "main": "",
+        "alt1": "",
+        "alt2": ""
+      },
+      "k11": {
+        "main": "<i class='fa fa-arrow-up'></i>",
+        "alt1": "",
+        "alt2": ""
+      },
+      "k12": {
+        "main": "Next",
+        "alt1": "",
+        "alt2": ""
+      },
+      "k13": {
+        "main": "",
+        "alt1": "",
+        "alt2": ""
+      },
+      "k14": {
+        "main": "",
+        "alt1": "",
+        "alt2": ""
+      },
+      "k15": {
+        "main": "",
+        "alt1": "",
+        "alt2": ""
+      },
+      "k16": {
+        "main": "<i class='fa fa-arrow-left'></i>",
+        "alt1": "",
+        "alt2": ""
+      },
+      "k17": {
+        "main": "<i class='fa fa-arrow-down'></i>",
+        "alt1": "",
+        "alt2": ""
+      },
+      "k18": {
+        "main": "<i class='fa fa-arrow-right'></i>",
+        "alt1": "",
+        "alt2": ""
+      },
+      "k19": {
+        "main": "A",
+        "alt1": "",
+        "alt2": ""
+      },
+      "k20": {
+        "main": "B",
+        "alt1": "",
+        "alt2": ""
+      },
+      "k21": {
+        "main": "C",
+        "alt1": "",
+        "alt2": ""
+      },
+      "k22": {
+        "main": "D",
+        "alt1": "",
+        "alt2": ""
+      },
+      "k23": {
+        "main": "E",
+        "alt1": "",
+        "alt2": ""
+      },
+      "k24": {
+        "main": "F",
+        "alt1": "",
+        "alt2": ""
+      },
+      "k25": {
+        "main": "",
+        "alt1": "",
+        "alt2": ""
+      },
+      "k26": {
+        "main": "7",
+        "alt1": "",
+        "alt2": ""
+      },
+      "k27": {
+        "main": "8",
+        "alt1": "",
+        "alt2": ""
+      },
+      "k28": {
+        "main": "9",
+        "alt1": "",
+        "alt2": ""
+      },
+      "k29": {
+        "main": "-",
+        "alt1": "",
+        "alt2": ""
+      },
+      "k30": {
+        "main": "<i class='a fa-reply'></i>",
+        "alt1": "",
+        "alt2": ""
+      },
+      "k31": {
+        "main": "4",
+        "alt1": "",
+        "alt2": ""
+      },
+      "k32": {
+        "main": "5",
+        "alt1": "",
+        "alt2": ""
+      },
+      "k33": {
+        "main": "6",
+        "alt1": "",
+        "alt2": ""
+      },
+      "k34": {
+        "main": "+",
+        "alt1": "",
+        "alt2": ""
+      },
+      "k35": {
+        "main": "<i class='fa fa-share'></i>",
+        "alt1": "",
+        "alt2": ""
+      },
+      "k36": {
+        "main": "1",
+        "alt1": "",
+        "alt2": ""
+      },
+      "k37": {
+        "main": "2",
+        "alt1": "",
+        "alt2": ""
+      },
+      "k38": {
+        "main": "3",
+        "alt1": "",
+        "alt2": ""
+      },
+      "k39": {
+        "main": "-",
+        "alt1": "",
+        "alt2": ""
+      },
+      "k40": {
+        "main": "+/-",
+        "alt1": "",
+        "alt2": ""
+      },
+      "k41": {
+        "main": "0",
+        "alt1": "Test",
+        "alt2": "Test2"
+      },
+      "k42": {
+        "main": ".",
+        "alt1": "",
+        "alt2": ""
+      },
+      "k43": {
+        "main": "Enter",
+        "alt1": "",
+        "alt2": ""
+      },
+      "k44": {
+        "main": "+",
+        "alt1": "",
+        "alt2": ""
+      }
+    }
 
 
-  $key.mousedown(function() {
-  });
+
+
+  $key.mousedown(function() {});
 
 });
