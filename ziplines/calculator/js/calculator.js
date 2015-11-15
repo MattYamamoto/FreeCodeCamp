64f5da0970e34685de500db0105a7276f5277077
MattYamamoto <matt.yamamoto@gmail.com>
2015-11-14 21:21:11 -0800

    Change key button IDs to be zero based
    well, that was stupid.

--- ziplines/calculator/js/calculator.js
+++ ziplines/calculator/js/calculator.js
@@ -1,6 +1,11 @@
 $(document).ready(function() {
   var $key = $('.key-button'),
     keyMap = {
+      "k6": {
+        "main": "",
+        "alt1": "",
+        "alt2": ""
+      },
       "k7": {
         "main": "",
         "alt1": "",
@@ -17,17 +22,17 @@ $(document).ready(function() {
         "alt2": ""
       },
       "k10": {
-        "main": "",
-        "alt1": "",
-        "alt2": ""
-      },
-      "k11": {
         "main": "<i class='fa fa-arrow-up'></i>",
         "alt1": "",
         "alt2": ""
       },
-      "k12": {
+      "k11": {
         "main": "Next",
+        "alt1": "",
+        "alt2": ""
+      },
+      "k12": {
+        "main": "",
         "alt1": "",
         "alt2": ""
       },
@@ -42,156 +47,156 @@ $(document).ready(function() {
         "alt2": ""
       },
       "k15": {
-        "main": "",
-        "alt1": "",
-        "alt2": ""
-      },
-      "k16": {
         "main": "<i class='fa fa-arrow-left'></i>",
         "alt1": "",
         "alt2": ""
       },
-      "k17": {
+      "k16": {
         "main": "<i class='fa fa-arrow-down'></i>",
         "alt1": "",
         "alt2": ""
       },
-      "k18": {
+      "k17": {
         "main": "<i class='fa fa-arrow-right'></i>",
         "alt1": "",
         "alt2": ""
       },
-      "k19": {
+      "k18": {
         "main": "A",
         "alt1": "",
         "alt2": ""
       },
-      "k20": {
+      "k19": {
         "main": "B",
         "alt1": "",
         "alt2": ""
       },
-      "k21": {
+      "k20": {
         "main": "C",
         "alt1": "",
         "alt2": ""
       },
-      "k22": {
+      "k21": {
         "main": "D",
         "alt1": "",
         "alt2": ""
       },
-      "k23": {
+      "k22": {
         "main": "E",
         "alt1": "",
         "alt2": ""
       },
-      "k24": {
+      "k23": {
         "main": "F",
         "alt1": "",
         "alt2": ""
       },
-      "k25": {
+      "k24": {
         "main": "",
         "alt1": "",
         "alt2": ""
       },
-      "k26": {
+      "k25": {
         "main": "7",
         "alt1": "",
         "alt2": ""
       },
-      "k27": {
+      "k26": {
         "main": "8",
         "alt1": "",
         "alt2": ""
       },
-      "k28": {
+      "k27": {
         "main": "9",
         "alt1": "",
         "alt2": ""
       },
-      "k29": {
+      "k28": {
         "main": "-",
         "alt1": "",
         "alt2": ""
       },
-      "k30": {
+      "k29": {
         "main": "<i class='a fa-reply'></i>",
         "alt1": "",
         "alt2": ""
       },
-      "k31": {
+      "k30": {
         "main": "4",
         "alt1": "",
         "alt2": ""
       },
-      "k32": {
+      "k31": {
         "main": "5",
         "alt1": "",
         "alt2": ""
       },
-      "k33": {
+      "k32": {
         "main": "6",
         "alt1": "",
         "alt2": ""
       },
-      "k34": {
+      "k33": {
         "main": "+",
         "alt1": "",
         "alt2": ""
       },
-      "k35": {
+      "k34": {
         "main": "<i class='fa fa-share'></i>",
         "alt1": "",
         "alt2": ""
       },
-      "k36": {
+      "k35": {
         "main": "1",
         "alt1": "",
         "alt2": ""
       },
-      "k37": {
+      "k36": {
         "main": "2",
         "alt1": "",
         "alt2": ""
       },
-      "k38": {
+      "k37": {
         "main": "3",
         "alt1": "",
         "alt2": ""
       },
-      "k39": {
+      "k38": {
         "main": "-",
         "alt1": "",
         "alt2": ""
       },
-      "k40": {
+      "k39": {
         "main": "+/-",
         "alt1": "",
         "alt2": ""
       },
-      "k41": {
+      "k40": {
         "main": "0",
         "alt1": "Test",
         "alt2": "Test2"
       },
-      "k42": {
+      "k41": {
         "main": ".",
         "alt1": "",
         "alt2": ""
       },
-      "k43": {
+      "k42": {
         "main": "Enter",
         "alt1": "",
         "alt2": ""
       },
-      "k44": {
+      "k43": {
         "main": "+",
         "alt1": "",
         "alt2": ""
       }
-    }
+    };
+console.log($key[1]);
+  function setKeys() {
+
+
+  }
 
 
 
